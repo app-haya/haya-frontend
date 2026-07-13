c301423f82bd5c0040c68e957af09a56cdba0a29
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2026-07-14 01:21:06 +0300

    feat: enable smooth scroll to download section in landing page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,25 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';import { provideRouter } from '@angular/router';import { provideHttpClient, HttpClient } from '@angular/common/http';import { TranslateModule, TranslateLoader } from '@ngx-translate/core';import { routes } from './app.routes';import { MyTranslateLoader } from './loaders/my-translate-loader';import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
-export const appConfig: ApplicationConfig = {  providers: [    provideRouter(routes),    provideHttpClient(),    importProvidersFrom(      TranslateModule.forRoot({        loader: {          provide: TranslateLoader,          useClass: MyTranslateLoader,          deps: [HttpClient],        },        defaultLanguage: 'en',      })    ),    provideCharts(withDefaultRegisterables()),
-  ]};
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withAnchorScrolling, withScrollPositionRestoration } from '@angular/router';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { routes } from './app.routes';
+import { MyTranslateLoader } from './loaders/my-translate-loader';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withAnchorScrolling(), withScrollPositionRestoration('enabled')),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: MyTranslateLoader,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'en',
+      })
+    ),
+    provideCharts(withDefaultRegisterables()),
+  ]
+};
